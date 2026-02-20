5703d02dd62addd594ad25b1ebf5c74960326c42
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-02-20 17:11:27 -0600

    Create plugin.py

---
+++ sentry/types/plugin.py
@@ -0,0 +1,8 @@
+from sentry.types import SlottedModel
+class PluginConfig(SlottedModel):
+    def load(self, obj, *args, **kwargs):
+        obj = {
+            k: v for k, v in obj.items()
+            if k in self._fields and not self._fields[k].metadata.get('private')
+        }
+        return super(PluginConfig, self).load(obj, *args, **kwargs)
