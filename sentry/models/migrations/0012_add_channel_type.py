202a658de272738a8d077f922d0bd8ead3dafe2a
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-02-20 15:14:27 -0600

    Update 0012_add_channel_type.py

--- sentry/models/migrations/0012_add_channel_type.py
+++ sentry/models/migrations/0012_add_channel_type.py
@@ -1,7 +1,5 @@
-from rowboat.models.migrations import Migrate
-from rowboat.models.channel import Channel
-
-
+from sentry.models.migrations import Migrate
+from sentry.models.channel import Channel
 @Migrate.only_if(Migrate.missing, Channel, 'type_')
 def add_channel_type_column(m):
     m.add_columns(Channel, Channel.type_)
