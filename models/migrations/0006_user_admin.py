73c25eac542fc66333aad08f3a7e7b3911c4c8fd
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-02-20 15:10:04 -0600

    Update 0006_user_admin.py

--- models/migrations/0006_user_admin.py
+++ models/migrations/0006_user_admin.py
@@ -1,7 +1,5 @@
-from rowboat.models.migrations import Migrate
-from rowboat.models.user import User 
-
-
+from sentry.models.migrations import Migrate
+from sentry.models.user import User 
 @Migrate.only_if(Migrate.missing, User, 'admin')
 def add_guild_columns(m):
     m.add_columns(User, User.admin)
