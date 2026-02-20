a6e5d8d69b96d92e78b894f85fb13df44d9beb82
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-02-20 15:05:26 -0600

    commit

---
+++ models/migrations/__init__.py
@@ -0,0 +1,7 @@
+from sentry.models.channel import *
+from sentry.models.event import *
+from sentry.models.guild import *
+from sentry.models.message import *
+from sentry.models.notification import *
+from sentry.models.user import *
+from sentry.models.tags import *
---
+++ models/migrations/tags.py
@@ -0,0 +1,16 @@
+from peewee import (
+    BigIntegerField, TextField, DateTimeField, CompositeKey, IntegerField
+)
+from datetime import datetime
+from sentry.sql import BaseModel
+@BaseModel.register
+class Tag(BaseModel):
+    guild_id = BigIntegerField()
+    author_id = BigIntegerField()
+    name = TextField()
+    content = TextField()
+    times_used = IntegerField(default=0)
+    created_at = DateTimeField(default=datetime.utcnow)
+    class Meta:
+        db_table = 'tags'
+        primary_key = CompositeKey('guild_id', 'name')
