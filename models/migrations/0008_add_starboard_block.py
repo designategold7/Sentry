015e44bc3d8345df34452bbc07dfec7140f2f332
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-02-20 15:10:36 -0600

    Update 0008_add_starboard_block.py

--- models/migrations/0008_add_starboard_block.py
+++ models/migrations/0008_add_starboard_block.py
@@ -1,7 +1,5 @@
-from rowboat.models.migrations import Migrate
-from rowboat.models.message import StarboardEntry
-
-
+from sentry.models.migrations import Migrate
+from sentry.models.message import StarboardEntry
 @Migrate.only_if(Migrate.missing, StarboardEntry, 'blocked_stars')
 def add_guild_columns(m):
     m.add_columns(
