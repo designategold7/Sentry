e93f47db79aa6ff1781327a153d2d728dee8bbbc
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-02-20 15:10:21 -0600

    Update 0007_add_infractions_metadata.py

--- models/migrations/0007_add_infractions_metadata.py
+++ models/migrations/0007_add_infractions_metadata.py
@@ -1,7 +1,5 @@
-from rowboat.models.migrations import Migrate
-from rowboat.models.user import Infraction
-
-
+from sentry.models.migrations import Migrate
+from sentry.models.user import Infraction
 @Migrate.only_if(Migrate.missing, Infraction, 'metadata')
 def add_guild_columns(m):
     m.add_columns(Infraction, Infraction.metadata)
