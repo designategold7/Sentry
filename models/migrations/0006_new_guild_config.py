556f055c88d3a4c5410e1a60ea8042c56e8ddeaa
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-02-20 15:09:59 -0600

    Update 0006_new_guild_config.py

--- models/migrations/0006_new_guild_config.py
+++ models/migrations/0006_new_guild_config.py
@@ -1,7 +1,5 @@
-from rowboat.models.migrations import Migrate
-from rowboat.models.guild import Guild
-
-
+from sentry.models.migrations import Migrate
+from sentry.models.guild import Guild
 @Migrate.only_if(Migrate.missing, Guild, 'config_raw')
 def add_guild_columns(m):
     m.add_columns(Guild, Guild.config_raw)
