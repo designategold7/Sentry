749840af1bc7adaa20600a1ba7fb4bf82b966439
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-02-21 11:23:35 -0600

    Create guild_stats.js

---
+++ frontend/src/components/guild_stats.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import {globalState} from '../state';
+import {withRouter} from 'react-router';
+import {PREMIUM_ENABLED} from 'config';
+export default class GuildStats extends Component {
+  render() {
+    return (<h1>TEST</h1>);
+  }
+}
