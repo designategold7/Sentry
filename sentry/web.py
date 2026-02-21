e11ca2d4866403cbf229236cf33aa35ed6b12c6b
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-02-21 15:29:07 -0600

    Update web.py

--- sentry/web.py
+++ sentry/web.py
@@ -1,4 +1,5 @@
-import os; os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+import os
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 import logging
 from flask import Flask, g, session
 from holster.flask_ext import Holster
@@ -6,23 +7,34 @@ from sentry import ENV
 from sentry.sql import init_db
 from sentry.models.user import User
 from sentry.types.guild import PluginsConfig
-from yaml import load
-sentry_app = Holster(Flask(__name__))
+from yaml import safe_load  
+raw_app = Flask(__name__)
+raw_app.logger_name = raw_app.name
+sentry_app = Holster(raw_app)
+# -------------------------------------
+
 logging.getLogger('peewee').setLevel(logging.DEBUG)
+
 @sentry_app.app.before_first_request
 def before_first_request():
     init_db(ENV)
     PluginsConfig.force_load_plugin_configs()
     with open('config.yaml', 'r') as f:
-        data = load(f)
-    sentry_app.app.config.update(data['web'])
-    sentry_app.app.secret_key = data['web']['SECRET_KEY']
+        data = safe_load(f) 
+    
+    web_config = data.get('web', {})
+    sentry_app.app.config.update(web_config)
+    
+ 
+    sentry_app.app.secret_key = web_config.get('SECRET_KEY', 'sentry_default_secret_key_8675309')
     sentry_app.app.config['token'] = data.get('token')
+
 @sentry_app.app.before_request
 def check_auth():
     g.user = None
     if 'uid' in session:
         g.user = User.with_id(session['uid'])
+
 @sentry_app.app.after_request
 def save_auth(response):
     if g.user and 'uid' not in session:
@@ -30,6 +42,7 @@ def save_auth(response):
     elif not g.user and 'uid' in session:
         del session['uid']
     return response
+
 @sentry_app.app.context_processor
 def inject_data():
     return dict(
