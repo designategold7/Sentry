6567f42a48a7c5f7d6a77f40b663e3783718ccf9
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-02-21 00:15:36 -0600

    Create users.py

---
+++ sentry/views/users.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, g, jsonify
+from sentry.models.guild import Guild
+from sentry.util.decos import authed
+users = Blueprint('users', __name__, url_prefix='/api/users')
+@users.route('/@me')
+@authed
+def users_me():
+    return jsonify(g.user.serialize(us=True))
+@users.route('/@me/guilds')
+@authed
+def users_me_guilds():
+    if g.user.admin:
+        guilds = list(Guild.select())
+    else:
+        guilds = list(Guild.select(
+            Guild,
+            Guild.config['web'][str(g.user.user_id)].alias('role')
+        ).where(
+            (~(Guild.config['web'][str(g.user.user_id)] >> None))
+        ))
+    return jsonify([
+        guild.serialize() for guild in guilds
+    ])
