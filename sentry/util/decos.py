f37c7e5051ab4155e207be9b79a42579944a2530
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-02-20 23:48:50 -0600

    Create decos.py

---
+++ sentry/util/decos.py
@@ -0,0 +1,15 @@
+from flask import g, jsonify
+from http.client import FORBIDDEN
+import functools
+def _authed(func):
+    @functools.wraps(func)
+    def deco(*args, **kwargs):
+        if not hasattr(g, 'user') or not g.user:
+            return jsonify({'error': 'Authentication Required'}), FORBIDDEN
+        return func(*args, **kwargs)
+    return deco
+def authed(func=None):
+    if callable(func):
+        return _authed(func)
+    else:
+        return functools.partial(_authed)
