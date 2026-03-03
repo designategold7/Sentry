4eb1862910981b66d6cd71b6a7d512df6418aa8f
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-03-03 15:09:08 -0600

    Update tags.py

--- sentry/models/tags.py
+++ sentry/models/tags.py
@@ -1,8 +1,9 @@
 from peewee import (
     BigIntegerField, TextField, DateTimeField, CompositeKey, IntegerField
 )
-from datetime import datetime
+from datetime import datetime, timezone
 from sentry.sql import BaseModel
+
 @BaseModel.register
 class Tag(BaseModel):
     guild_id = BigIntegerField()
@@ -10,7 +11,8 @@ class Tag(BaseModel):
     name = TextField()
     content = TextField()
     times_used = IntegerField(default=0)
-    created_at = DateTimeField(default=datetime.utcnow)
+    created_at = DateTimeField(default=lambda: datetime.now(timezone.utc).replace(tzinfo=None))
+
     class Meta:
         db_table = 'tags'
         primary_key = CompositeKey('guild_id', 'name')
