b547c39e25ecebc736a0609293cdb09c8e0691ef
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-03-03 15:08:14 -0600

    Update 0003_nullable_avatar.py

--- sentry/models/migrations/0003_nullable_avatar.py
+++ sentry/models/migrations/0003_nullable_avatar.py
@@ -1,5 +1,6 @@
 from sentry.models.migrations import Migrate
-from sentry.models.guild import User
+from sentry.models.user import User # FIXED: Was erroneously importing from models.guild
+
 @Migrate.only_if(Migrate.non_nullable, User, 'avatar')
 def alter_guild_columns(m):
     m.drop_not_nulls(User, User.avatar)
