9ec6c435c20017fed528922fda0c927d0ff72205
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-03-03 15:09:34 -0600

    Update leakybucket.py

--- sentry/util/leakybucket.py
+++ sentry/util/leakybucket.py
@@ -1,12 +1,8 @@
 import time
-def get_ms_time():
-    return int(time.time() * 1000)
-# function(keys=[rl_key], args=[time.time() - (time_period * max_actions), time.time()]
+def get_ms_time(): return int(time.time() * 1000)
 INCR_SCRIPT = '''
 local key = KEYS[1]
--- Clear out expired water drops
 redis.call("ZREMRANGEBYSCORE", KEYS[1], "-inf", ARGV[2])
--- Add our keys
 for i=1,ARGV[1] do
   redis.call("ZADD", KEYS[1], ARGV[3], ARGV[3] + i)
 end
@@ -15,7 +11,6 @@ return redis.call("ZCOUNT", KEYS[1], "-inf", "+inf")
 '''
 GET_SCRIPT = '''
 local key = KEYS[1]
--- Clear out expired water drops
 redis.call("ZREMRANGEBYSCORE", KEYS[1], "-inf", ARGV[1])
 return redis.call("ZCOUNT", KEYS[1], "-inf", "+inf")
 '''
@@ -29,19 +24,9 @@ class LeakyBucket(object):
         self._get_script = self.redis.register_script(GET_SCRIPT)
     def incr(self, key, amount=1):
         key = self.key_fmt.format(key)
-        return int(self._incr_script(
-            keys=[key],
-            args=[
-                amount,
-                get_ms_time() - self.time_period,
-                get_ms_time(),
-                (self.time_period * 2) / 1000,
-            ]))
+        return int(self._incr_script(keys=[key], args=[amount, get_ms_time() - self.time_period, get_ms_time(), (self.time_period * 2) / 1000]))
     def check(self, key, amount=1):
-        count = self.incr(key, amount)
-        if count >= self.max_actions:
-            return False
-        return True
+        return self.incr(key, amount) < self.max_actions
     def get(self, key):
         return int(self._get_script(self.key_fmt.format(key)))
     def clear(self, key):
@@ -50,6 +35,5 @@ class LeakyBucket(object):
         return self.redis.zcount(self.key_fmt.format(key), '-inf', '+inf')
     def size(self, key):
         res = list(map(int, self.redis.zrangebyscore(self.key_fmt.format(key), '-inf', '+inf')))
-        if len(res) <= 1:
-            return 0
+        if len(res) <= 1: return 0
         return (res[-1] - res[0]) / 1000.0
