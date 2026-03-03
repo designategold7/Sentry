b231ca8e42c190f39e2ba739bf21d738a10131fb
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-03-03 15:07:47 -0600

    Update nulld.py

--- docker/nulld.py
+++ docker/nulld.py
@@ -1,10 +1,4 @@
 import socket
-
-UDP_IP = "0.0.0.0"
-UDP_PORT = 8125
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
-
-while True:
-    sock.recvfrom(1024)
+sock.bind(("0.0.0.0", 8125))
+while True: sock.recvfrom(1024)
