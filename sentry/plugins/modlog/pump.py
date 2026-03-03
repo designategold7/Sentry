bde7d7cee63fb1e1fff66f8fdf7cfff62f835030
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-03-03 15:09:18 -0600

    Update pump.py

--- sentry/plugins/modlog/pump.py
+++ sentry/plugins/modlog/pump.py
@@ -1,65 +1,72 @@
 import time
-import gevent
-from disco.api.http import APIException
-from disco.util.logging import LoggingClass
-class ModLogPump(LoggingClass):
-    def __init__(self, channel, sleep_duration=5):
+import asyncio
+import discord
+
+class ModLogPump:
+    def __init__(self, bot, channel, sleep_duration=5):
+        self.bot = bot
         self.channel = channel
         self.sleep_duration = sleep_duration
         self._buffer = []
-        self._have = gevent.event.Event()
+        self._have = asyncio.Event()
+        self._lock = asyncio.Lock()
         self._quiescent_period = None
-        self._lock = gevent.lock.Semaphore()
-        self._greenlet = gevent.spawn(self._emitter_loop)
-    def _start_emitter(self, greenlet=None):
-        self.log.warning('Restarting emitter for ModLogPump %s' % self.channel)
-        self._greenlet = gevent.spawn(self._emitter_loop)
-        self._greenlet.link_exception(self._start_emitter)
+        self._task = self.bot.loop.create_task(self._emitter_loop())
+
     def __del__(self):
-        if self._greenlet:
-            self._greenlet.kill()
-    def _emitter_loop(self):
+        if hasattr(self, '_task') and not self._task.done():
+            self._task.cancel()
+
+    async def _emitter_loop(self):
         while True:
-            self._have.wait()
+            await self._have.wait()
             backoff = False
-            with self.channel.client.api.capture() as responses:
-                try:
-                    self._emit()
-                except APIException as e:
-                    # Message send is disabled
-                    if e.code == 40004:
-                        backoff = True
-                except Exception:
-                    self.log.exception('Exception when executing ModLogPump._emit: ')
-            if responses.rate_limited:
-                backoff = True
-            # If we need to backoff, set a quiescent period that will batch
-            #  requests for the next 60 seconds.
+
+            try:
+                await self._emit()
+            except discord.HTTPException as e:
+                # 429 Too Many Requests or 40004 Send Disabled
+                if e.status == 429 or e.code == 40004:
+                    backoff = True
+            except Exception as e:
+                print(f'Exception when executing ModLogPump._emit: {e}')
+
             if backoff:
                 self._quiescent_period = time.time() + 60
+
             if self._quiescent_period:
                 if self._quiescent_period < time.time():
                     self._quiescent_period = None
                 else:
-                    gevent.sleep(self.sleep_duration)
-            if not self._buffer:
-                self._have.clear()
-    def _emit(self):
-        with self._lock:
+                    await asyncio.sleep(self.sleep_duration)
+
+            async with self._lock:
+                if not self._buffer:
+                    self._have.clear()
+
+    async def _emit(self):
+        async with self._lock:
             msg = self._get_next_message()
             if not msg:
                 return
-            self.channel.send_message(msg)
+                
+        # Send outside the lock to prevent holding up the buffer during network IO
+        await self.channel.send(msg)
+
     def _get_next_message(self):
         data = ''
         while self._buffer:
             payload = self._buffer.pop(0)
+            # Discord max message size is 2000
             if len(data) + (len(payload) + 1) > 2000:
+                self._buffer.insert(0, payload) # Push back if it overflows
                 break
-            data += '\n'
+            if data:
+                data += '\n'
             data += payload
         return data
-    def send(self, payload):
-        with self._lock:
+
+    async def send(self, payload):
+        async with self._lock:
             self._buffer.append(payload)
-            self._have.set()    
+            self._have.set()
