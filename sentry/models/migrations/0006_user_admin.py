20b484edae234be2b50a80aae760f682e48c783a
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-03-03 15:08:22 -0600

    Update 0006_user_admin.py

--- sentry/models/migrations/0006_user_admin.py
+++ sentry/models/migrations/0006_user_admin.py
@@ -1,5 +1,6 @@
 from sentry.models.migrations import Migrate
 from sentry.models.user import User 
+
 @Migrate.only_if(Migrate.missing, User, 'admin')
 def add_guild_columns(m):
     m.add_columns(User, User.admin)
