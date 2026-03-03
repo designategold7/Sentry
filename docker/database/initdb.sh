9146ba52f8b78ec941d7de31f48df1eee190c277
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-03-03 15:07:42 -0600

    Update initdb.sh

--- docker/database/initdb.sh
+++ docker/database/initdb.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
-
 psql -v ON_ERROR_STOP=1 --username "postgres" -d sentry -c "CREATE EXTENSION hstore;"
 psql -v ON_ERROR_STOP=1 --username "postgres" -d sentry -c "CREATE EXTENSION pg_trgm;"
