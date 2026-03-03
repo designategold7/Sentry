581869515dc481426f3dad85c907c0455201b76d
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-03-03 15:08:16 -0600

    Update 0006_new_guild_config.py

--- sentry/models/migrations/0006_new_guild_config.py
+++ sentry/models/migrations/0006_new_guild_config.py
@@ -1,5 +1,6 @@
 from sentry.models.migrations import Migrate
 from sentry.models.guild import Guild
+
 @Migrate.only_if(Migrate.missing, Guild, 'config_raw')
 def add_guild_columns(m):
     m.add_columns(Guild, Guild.config_raw)
