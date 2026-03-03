c4c3599300e608a7a57382d572559b09f3cb3673
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-03-03 15:07:58 -0600

    Update channel.py

--- sentry/models/channel.py
+++ sentry/models/channel.py
@@ -1,6 +1,7 @@
 from peewee import (BigIntegerField, SmallIntegerField, CharField, TextField, BooleanField)
 from sentry.sql import BaseModel
 from sentry.models.message import Message
+
 @BaseModel.register
 class Channel(BaseModel):
     channel_id = BigIntegerField(primary_key=True)
@@ -11,8 +12,10 @@ class Channel(BaseModel):
     # First message sent in the channel
     first_message_id = BigIntegerField(null=True)
     deleted = BooleanField(default=False)
+
     class Meta:
         db_table = 'channels'
+
     @classmethod
     def generate_first_message_id(cls, channel_id):
         try:
@@ -21,18 +24,23 @@ class Channel(BaseModel):
             ).order_by(Message.id.asc()).limit(1).get().id
         except Message.DoesNotExist:
             return None
+
     @classmethod
     def from_disco_channel(cls, channel):
+        # Extract d.py channel type enum safely
+        c_type = channel.type.value if hasattr(channel, 'type') else None
+        
         # Upsert channel information
-        channel = list(cls.insert(
+        chan_obj = list(cls.insert(
             channel_id=channel.id,
-            guild_id=channel.guild.id if channel.guild else None,
-            name=channel.name or None,
-            topic=channel.topic or None,
-            type_=channel.type,
+            guild_id=channel.guild.id if getattr(channel, 'guild', None) else None,
+            name=getattr(channel, 'name', None),
+            topic=getattr(channel, 'topic', None),
+            type_=c_type,
         ).upsert(target=cls.channel_id).returning(cls.first_message_id).execute())[0]
+        
         # Update the first message ID
-        if not channel.first_message_id:
+        if not chan_obj.first_message_id:
             cls.update(
                 first_message_id=cls.generate_first_message_id(channel.id)
             ).where(cls.channel_id == channel.id).execute()
