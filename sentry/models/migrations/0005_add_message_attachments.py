6b8fe1bd59c4c51cdfc2e0262bca351c8d9efce0
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-03-03 15:08:19 -0600

    Update 0005_add_message_attachments.py

--- sentry/models/migrations/0005_add_message_attachments.py
+++ sentry/models/migrations/0005_add_message_attachments.py
@@ -1,5 +1,6 @@
 from sentry.models.migrations import Migrate
 from sentry.models.message import Message
+
 @Migrate.only_if(Migrate.missing, Message, 'attachments')
 def add_guild_columns(m):
     m.add_columns(Message, Message.attachments)
