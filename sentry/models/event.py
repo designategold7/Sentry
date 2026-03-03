b2a9873118fbe8047a2ae370903983d6b7c74d14
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-03-03 15:08:04 -0600

    Update event.py

--- sentry/models/event.py
+++ sentry/models/event.py
@@ -1,16 +1,18 @@
 from peewee import (
     BigIntegerField, CharField, DateTimeField, CompositeKey
 )
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from playhouse.postgres_ext import BinaryJSONField
 from sentry.sql import BaseModel
+
 @BaseModel.register
 class Event(BaseModel):
     session = CharField()
     seq = BigIntegerField()
-    timestamp = DateTimeField(default=datetime.utcnow)
+    timestamp = DateTimeField(default=lambda: datetime.now(timezone.utc).replace(tzinfo=None))
     event = CharField()
     data = BinaryJSONField()
+
     class Meta:
         db_table = 'events'
         primary_key = CompositeKey('session', 'seq')
@@ -18,17 +20,20 @@ class Event(BaseModel):
             (('timestamp', ), False),
             (('event', ), False),
         )
+
     @classmethod
     def truncate(cls, hours=12):
+        now = datetime.now(timezone.utc).replace(tzinfo=None)
         return cls.delete().where(
-            (cls.timestamp < (datetime.utcnow() - timedelta(hours=hours)))
+            (cls.timestamp < (now - timedelta(hours=hours)))
         ).execute()
+
     @classmethod
     def prepare(cls, session, event):
         return {
             'session': session,
             'seq': event['s'],
-            'timestamp': datetime.utcnow(),
+            'timestamp': datetime.now(timezone.utc).replace(tzinfo=None),
             'event': event['t'],
             'data': event['d'],
         }
