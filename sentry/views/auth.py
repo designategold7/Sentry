d26b3a2beefdd9872bf9a3017208ad0490fbd807
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-03-03 15:12:15 -0600

    Update auth.py

--- sentry/views/auth.py
+++ sentry/views/auth.py
@@ -3,21 +3,9 @@ from requests_oauthlib import OAuth2Session
 from sentry.models.user import User
 from sentry.util.decos import authed
 auth = Blueprint('auth', __name__, url_prefix='/api/auth')
-def token_updater(token):
-    pass
+def token_updater(token): pass
 def make_discord_session(token=None, state=None, scope=None):
-    return OAuth2Session(
-        client_id=current_app.config['DISCORD_CLIENT_ID'],
-        token=token,
-        state=state,
-        scope=scope,
-        redirect_uri=current_app.config['DISCORD_REDIRECT_URI'],
-        auto_refresh_kwargs={
-            'client_id': current_app.config['DISCORD_CLIENT_ID'],
-            'client_secret': current_app.config['DISCORD_CLIENT_SECRET'],
-        },
-        auto_refresh_url=current_app.config['DISCORD_TOKEN_URL'],
-        token_updater=token_updater)
+    return OAuth2Session(client_id=current_app.config['DISCORD_CLIENT_ID'], token=token, state=state, scope=scope, redirect_uri=current_app.config['DISCORD_REDIRECT_URI'], auto_refresh_kwargs={'client_id': current_app.config['DISCORD_CLIENT_ID'], 'client_secret': current_app.config['DISCORD_CLIENT_SECRET']}, auto_refresh_url=current_app.config['DISCORD_TOKEN_URL'], token_updater=token_updater)
 @auth.route('/logout', methods=['POST'])
 @authed
 def auth_logout():
@@ -31,22 +19,14 @@ def auth_discord():
     return redirect(auth_url)
 @auth.route('/discord/callback')
 def auth_discord_callback():
-    if request.values.get('error'):
-        return request.values['error']
-    if 'state' not in session:
-        return 'no state', 400
+    if request.values.get('error'): return request.values['error']
+    if 'state' not in session: return 'no state', 400
     discord = make_discord_session(state=session['state'])
-    token = discord.fetch_token(
-        current_app.config['DISCORD_TOKEN_URL'],
-        client_secret=current_app.config['DISCORD_CLIENT_SECRET'],
-        authorization_response=request.url)
+    token = discord.fetch_token(current_app.config['DISCORD_TOKEN_URL'], client_secret=current_app.config['DISCORD_CLIENT_SECRET'], authorization_response=request.url)
     discord = make_discord_session(token=token)
     data = discord.get(current_app.config['DISCORD_API_BASE_URL'] + '/users/@me').json()
     user = User.with_id(data['id'])
-    if not user:
-        return 'Unknown User', 403
-    # if not user.admin:
-    #     return 'Invalid User', 403
+    if not user: return 'Unknown User', 403
     g.user = user
     return redirect('/')
 @auth.route('/@me')
