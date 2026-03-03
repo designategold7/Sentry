214cb774e51e618abbee0608990afc6b3a2dffcc
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-03-03 15:09:31 -0600

    Update gevent.py

--- sentry/util/gevent.py
+++ sentry/util/gevent.py
@@ -1,12 +1,8 @@
-from __future__ import absolute_import
-import gevent
-def wait_many(*args, **kwargs):
-    def _async():
-        for awaitable in args:
-            awaitable.wait()
-    gevent.spawn(_async).get(timeout=kwargs.get('timeout', None))
+import asyncio
+async def wait_many(*args, **kwargs):
+    results = await asyncio.gather(*args, return_exceptions=True)
     if kwargs.get('track_exceptions', True):
         from sentry import raven_client
-        for awaitable in args:
-            if awaitable.exception:
-                raven_client.captureException(exc_info=awaitable.exc_info)
+        for result in results:
+            if isinstance(result, Exception): raven_client.captureException(exc_info=result)
+    return results
