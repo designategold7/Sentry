0ddf636232eaa39d6d99a7c2bfc2b80328056736
designategold7 <17305941+designategold7@users.noreply.github.com>
2026-03-03 15:09:45 -0600

    Update stats.py

--- sentry/util/stats.py
+++ sentry/util/stats.py
@@ -2,17 +2,12 @@ import time
 from contextlib import contextmanager
 from datadog import statsd
 def to_tags(obj=None, **kwargs):
-    if obj:
-        kwargs.update(obj)
-    return ['{}:{}'.format(k, v) for k, v in kwargs.items()]
+    if obj: kwargs.update(obj)
+    return [f'{k}:{v}' for k, v in kwargs.items()]
 @contextmanager
 def timed(metricname, tags=None):
     start = time.time()
-    try:
-        yield
-    except:
-        raise
+    try: yield
     finally:
-        if tags and isinstance(tags, dict):
-            tags = to_tags(tags)
+        if tags and isinstance(tags, dict): tags = to_tags(tags)
         statsd.timing(metricname, (time.time() - start) * 1000, tags=tags)
